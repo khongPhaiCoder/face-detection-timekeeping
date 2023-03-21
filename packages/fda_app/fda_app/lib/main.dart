7d245d5c992533914f526c07c4a34394d0b93038
Nguyen Tan Khoa <khoa699602@gmail.com>
2023-03-21 16:53:47 +0700

    big update

---
+++ packages/fda_app/fda_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:fda_app/Screens/Login/login.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FlutterDemo',
+      theme: ThemeData(),
+      home: const LoginPage(),
+    );
+  }
+}
